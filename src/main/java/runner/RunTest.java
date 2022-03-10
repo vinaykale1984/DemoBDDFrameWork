ceabec10b32d177ec414c9ab06a702eb4ad899a4
vinaykale1984 <39239511+vinaykale1984@users.noreply.github.com>
2022-03-10 20:51:27 +0530

    Update RunTest.java

--- src/main/java/runner/RunTest.java
+++ src/main/java/runner/RunTest.java
@@ -6,10 +6,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-/**
- * @author CHIRAG
- *
- */
 
 // Uncomment @RunWith if you are using Junit to run Test 
 // @RunWith(Cucumber.class)
